dfa8fecb3d6276e1a357fccb0eae81798a712ac6
DinoWithCurls <adityarajsingh64@gmail.com>
2022-07-23 23:59:22 +0530

    feat: 🎸 Created the Add button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import StatusModal from './components/statusModal';
+import AddTaskButton from './components/addTaskButton'
 import './styles/App.css';
 function App() {
   
   return (
     <div className="App">
       <StatusModal />
+      <AddTaskButton />
     </div>
   );
 }
--- src/components/addTaskButton.js
+++ src/components/addTaskButton.js
@@ -1,7 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import "../styles/addButton.css";
+import { PlusCircleIcon } from '@heroicons/react/outline'
 const AddTaskButton = () => {
-    return <div>Add Task</div>
-}
+  return (
+    <div class="button" onClick={()=>console.log('Button Presse3d')}>
+      <div class="icon-container">
+        <PlusCircleIcon className="icon" />
+      </div>
+      <div class="text-container">
+        <div class="text">New Task</div>
+      </div>
+    </div>
+  );
+};
 
 export default AddTaskButton;
