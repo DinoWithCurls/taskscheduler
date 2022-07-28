52624fa77629347be6a694dc2155363b633aabed
DinoWithCurls <adityarajsingh64@gmail.com>
2022-07-28 20:31:11 +0530

    style: 💄 Fixed icons

--- src/components/iconsComponent.js
+++ src/components/iconsComponent.js
@@ -1,25 +1,22 @@
-import {
-  FilterIcon,
-  SelectorIcon,
-  XIcon,
-} from "@heroicons/react/solid";
-import { PlusCircleIcon } from "@heroicons/react/outline";
-
 import NestCamWiredStandIcon from '@mui/icons-material/NestCamWiredStand';
 import CallIcon from '@mui/icons-material/Call';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import CloseIcon from '@mui/icons-material/Close';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+
 export {
   KeyboardArrowDownIcon as Down,
   CallIcon as Phone,
   LocationOnIcon as Location,
-  FilterIcon as Filter,
-  SelectorIcon as Selector,
-  PlusCircleIcon as Add,
+  FilterAltIcon as Filter,
+  AddCircleOutlineIcon as Add,
   ChevronRightIcon as Right,
-  XIcon as Cross,
+  CloseIcon as Cross,
   NestCamWiredStandIcon as Webcam,
   SearchIcon as Search
 };
