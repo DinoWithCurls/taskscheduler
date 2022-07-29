aa80397bc7314fda218e319806f7a07af4eb9e68
DinoWithCurls <adityarajsingh64@gmail.com>
2022-07-29 05:41:51 +0530

    fix: 🐛 Fixed search bar UI

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Search } from "../utils/iconsComponent";
 import "../styles/searchbar.css";
 function Searchbar() {
   return (
     <div class="body">
-      <Search />
       <input class="search" placeholder="Search" />
     </div>
   );
